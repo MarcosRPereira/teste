35c9e2ef72883caa0207d37361609bd4f3128bf8
Marcos Rafael Pereira <62770109+MarcosRPereira@users.noreply.github.com>
2021-02-25 12:26:00 -0300

    Add files via upload

--- anime.js
+++ anime.js
@@ -2,11 +2,15 @@ let teste = fetch('./teste.json');
 
 teste.then(resolve =>{
   console.log(resolve)
-  return resolve.text()
+  return resolve.json();
 })
 .then(resposta =>{
-  const conteudo = document.querySelector('.conteudo');
-  conteudo.innerHTML = resposta;
   console.log(resposta)
+  const conteudo = document.querySelector('.conteudo');
+  conteudo.innerHTML = resposta[0].Anime +" "+ resposta[0].Episodio +" "+ resposta[0].Status + "<br>" +
+  resposta[1].Anime +" "+ resposta[1].Episodio +" "+ resposta[1].Status + "<br>" +
+  resposta[2].Anime +" "+ resposta[2].Episodio +" "+ resposta[2].Status + "<br>" +
+  resposta[3].Anime +" "+ resposta[3].Episodio +" "+ resposta[3].Status;
+  console.log(resposta.Anime, resposta.Episodio, resposta.Status)
 })
 
